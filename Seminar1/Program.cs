eb1c7c32d5b7bc3c3d79d8d5a0e174013a5f67eb
Alejandro Yermakov <cakkoc@yandex.ru>
2023-07-24 01:50:52 +0500

    seminar1 conspect added

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");*/
+
+/* 
+int intNumber = 2;
+double doubleNumber = 12.2;
+string text = "some text";
+bool logicVar = true;
+*/
+
+/*int num1 = 5;
+int num2 = 10;
+Cosole.WriteLine("My numbers are " + num1 + num2 + "and it is good!");
+Cosole.WriteLine($"My numbers are {num1} and {num2} and it is good!");
+*/
+
+/*
+Ввод и вывод данных 
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your number is " + num);
+*/
+
+// Task 1.
+
+//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+/*
+Console.Write("Input a first number: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num_2 * num_2;
+
+if(num_1 == quad2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+
+// Если команда в if/else одна, можжно обойтись без скобок
+*/
+
+
+// Task 2. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+/*
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = -number;
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current++;
+}
+
+*/
+// HomeWork added:
+
+
